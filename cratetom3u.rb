970b61b035e464015ace9ebc65b1674cc23b9d84
Martin Hansen <code@martin-hansen.de>
2017-02-17 15:45:43 +0100

    cratetom3u 0.2.0

--- cratetom3u.rb
+++ cratetom3u.rb
@@ -1,25 +1,25 @@
 
 class Cratetom3u < Formula
   desc "Cratetom3u is a tool to convert Serato .crate files to .m3u playlist files."
-  homepage ""
-  url "https://github.com/MartinHH/CrateToM3U/releases/download/v0.1.0/io.github.martinhh.cratetom3u-0.1.0.jar"
-  sha256 "129c7e8772c07553928be24744eff65d274bd1d4d56268ae4ff81344aaceeecf"
+  homepage "https://github.com/MartinHH/CrateToM3U"
+  url "https://github.com/MartinHH/CrateToM3U/releases/download/v0.2.0/io.github.martinhh.cratetom3u-0.2.0.jar"
+  sha256 "2caa1ae78e6c7d85c8ccfbb5b291cfd3c315f28983ad62b6a4cd7b5108529fd4"
 
-  depends_on :java => "1.8+"
+  depends_on :java => "1.7+"
 
   resource "scallop" do
-    url "https://repo1.maven.org/maven2/org/rogach/scallop_2.12/2.0.7/scallop_2.12-2.0.7.jar"
-    sha256 "21b93ec50df3279899fd3756fe9f93bdbdb89f9bd39bc4e6d10eeb90b2caf545"
+    url "https://repo1.maven.org/maven2/org/rogach/scallop_2.11/2.0.7/scallop_2.11-2.0.7.jar"
+    sha256 "b37f2c05192b0fce5086c973c3ea47bbb95959c44a7903e341e1d7120fef70e8"
   end
 
   resource "scala-library" do
-    url "https://repo1.maven.org/maven2/org/scala-lang/scala-library/2.12.1/scala-library-2.12.1.jar"
-    sha256 "9dab78f3f205a038f48183b2391f8a593235f794d8129a479e06af3e6bc50ef8"
+    url "https://repo1.maven.org/maven2/org/scala-lang/scala-library/2.11.8/scala-library-2.11.8.jar"
+    sha256 "401e0f47d63221c811964534f2e480169f50919c804f728930ac6037eca4e5f6"
   end  
 
   resource "scala-reflect" do
-    url "https://repo1.maven.org/maven2/org/scala-lang/scala-reflect/2.12.1/scala-reflect-2.12.1.jar"
-    sha256 "d8a2b9d6d78c7457a40e394dc0c4fa6d6244acf0d156bbbcb311a9d497b85eec"
+    url "https://repo1.maven.org/maven2/org/scala-lang/scala-reflect/2.11.8/scala-reflect-2.11.8.jar"
+    sha256 "29e081446a2a35de867411e06c6bc86863ac802401f8e8826f87723f668b4319"
   end   
 
   def install
@@ -50,8 +50,8 @@ class Cratetom3u < Formula
   test do
     # Test expected output when running cratetom3u with an empty input file
     (testpath/"Foo.crate").write("")
-    output=shell_output("#{bin}/cratetom3u -i #{testpath}/Foo.crate -o Bar.m3u")
-    expected="[CrateToM3U]: Error: no tracks found."
+    output=shell_output("#{bin}/cratetom3u -f #{testpath}/Foo.crate Bar.m3u")
+    expected="[cratetom3u]: No tracks found"
     assert(output.start_with? expected)
   end
 end
